786d0318bc00f1951ee148f79329a9222762d470
Trevor Rounce <trevorrounce@Trevors-iMac-2.hitronhub.home>
2019-06-05 15:31:34 -0600

    netlify plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,10 @@ module.exports = {
         useACF: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {},
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
