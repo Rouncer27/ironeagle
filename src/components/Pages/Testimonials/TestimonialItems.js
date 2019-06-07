a4b82ca6e14b4c564a0a6f0d49be9c8180fa9694
Trevor Rounce <trevorrounce@Trevors-iMac.local>
2019-06-07 11:43:29 -0600

    made the rows

--- src/components/Pages/Testimonials/TestimonialItems.js
+++ src/components/Pages/Testimonials/TestimonialItems.js
@@ -7,28 +7,49 @@ import { StandardWrapper } from "../../styles/Commons/Wrappers"
 import { AngleGreyBackground } from "../../styles/Commons/Effects"
 
 const TestimonialItemsStyled = styled.div`
-  position: relative;
+  .test-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    position: relative;
+    width: 100%;
+  }
 `
 
 const TestimonialItems = props => {
   const { tests } = props
+  let oneRow = []
+  const rowsOfTest = []
+
+  tests.forEach((test, index) => {
+    if ((index + 1) % 3 === 0) {
+      oneRow.push(test)
+      rowsOfTest.push(oneRow)
+      oneRow = []
+    } else {
+      oneRow.push(test)
+    }
+  })
+
   return (
     <TestimonialItemsStyled>
       <StandardWrapper>
-        {tests &&
-          tests.length > 0 &&
-          tests.map((test, index) => {
-            const BG = (index + 1) % 3 === 0 ? true : false
-
-            console.log(BG, index)
-            return (
-              <TestItem
-                key={index}
-                content={test.node.acf._ire_test_content}
-                name={test.node.acf._ire_test_name}
-              />
-            )
-          })}
+        {rowsOfTest.map((row, index) => {
+          return (
+            <div key={index} className="test-row">
+              {row.map((test, index) => {
+                return (
+                  <TestItem
+                    key={index}
+                    content={test.node.acf._ire_test_content}
+                    name={test.node.acf._ire_test_name}
+                  />
+                )
+              })}
+              <AngleGreyBackground />
+            </div>
+          )
+        })}
       </StandardWrapper>
     </TestimonialItemsStyled>
   )
