7cc7ebf12fa19163707ff78860bf99f0804e0cdd
Trevor Rounce <trevorrounce@Trevors-iMac.local>
2019-06-18 10:47:37 -0600

    SEO descriptions and titles

--- src/templates/Services.js
+++ src/templates/Services.js
@@ -8,13 +8,88 @@ import TraneProducts from "../components/Pages/Services/TraneProducts"
 import ServicesLinks from "../components/Pages/Services/ServicesLinks"
 
 const AirConditioning = props => {
+  let serviceType = false
+  let metaTitle = "Air Conditioning. Heating. Refrigeration. - Iron Eagle"
+  let metaDesc =
+    "Meeting all of your air conditioning needs. Your comfort is our priority. Keeping your products at a safe temperature."
+  let metaKeys = ["air conditioning", "heating", "furnace", "refrigeration"]
+
+  if (
+    props.location.pathname === "/what-we-do/air-conditioning" ||
+    props.location.pathname === "/what-we-do/air-conditioning/"
+  ) {
+    serviceType = "ac"
+  } else if (
+    props.location.pathname === "/what-we-do/heating" ||
+    props.location.pathname === "/what-we-do/heating/"
+  ) {
+    serviceType = "heat"
+  } else if (
+    props.location.pathname === "/what-we-do/refrigeration" ||
+    props.location.pathname === "/what-we-do/refrigeration/"
+  ) {
+    serviceType = "ref"
+  }
+
+  if (serviceType === "ac") {
+    metaTitle = "Air Conditioning - Meeting all of your air conditioning needs."
+    metaDesc =
+      "We ensure that you are comfortable and cool with all installation, repair, and maintenance of both residential and commercial air conditioning systems."
+    metaKeys = [
+      "air conditioning",
+      "comfortable",
+      "cool",
+      "installation",
+      "maintenance",
+      "repair",
+      "residential",
+      "commercial",
+      "systems",
+      "function",
+      "call us",
+      "free quote",
+    ]
+  } else if (serviceType === "heat") {
+    metaTitle = "Heating - Your comfort is our priority."
+    metaDesc =
+      "Our trained technicians ensure that your furnace works efficiently when you need it.We install, repair, and maintain your furnace, including 24/7 Emergency Services."
+    metaKeys = [
+      "heating",
+      "furnace",
+      "installation",
+      "repair",
+      "maintenance",
+      "new furnace",
+      "energy efficient products",
+      "emergency repair",
+      "furnace checklist",
+      "sustainable",
+      "free quote",
+    ]
+  } else if (serviceType === "ref") {
+    metaTitle = "Refrigeration - Keeping your products at a safe temperature."
+    metaDesc =
+      "Our work includes commercial refrigeration projects. We install, repair, and maintain your commercial cooling systems including refrigerators, reach-in coolers, and freezers."
+    metaKeys = [
+      "refrigeration",
+      "cool",
+      "household",
+      "commercial",
+      "efficient",
+      "safe temperature",
+      "installation",
+      "repair",
+      "maintenance",
+      "systems",
+      "freezers",
+      "coolers",
+      "free quote",
+    ]
+  }
+
   return (
     <Layout location={props.location}>
-      <SEO
-        title="Air Conditioning. Heating. Refrigeration. - Iron Eagle"
-        description="Meeting all of your air conditioning needs. Your comfort is our priority. Keeping your products at a safe temperature."
-        keywords={["air conditioning", "heating", "furnace", "refrigeration"]}
-      />
+      <SEO title={metaTitle} description={metaDesc} keywords={metaKeys} />
       <Intro location={props.location} />
       <TraneProducts />
       <Types />
